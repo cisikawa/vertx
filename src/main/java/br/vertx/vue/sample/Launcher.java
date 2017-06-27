5bb6c0dc7bfc23acc6e26da571dcff3667cd7aa6
Caio Kiyoshi Isikawa <caiok@lnxcit016955.cit>
2017-06-27 11:41:56 -0300

    verticle rx e config jdbc

--- src/main/java/br/vertx/vue/sample/Launcher.java
+++ src/main/java/br/vertx/vue/sample/Launcher.java
@@ -2,6 +2,7 @@ package br.vertx.vue.sample;
 
 import java.io.IOException;
 
+import br.vertx.vue.sample.verticle.MyFirstRXVerticle;
 import br.vertx.vue.sample.verticle.MyFirstVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
@@ -10,8 +11,11 @@ import io.vertx.core.json.JsonObject;
 public class Launcher {
 
 	public static void main(final String[] args) throws IOException {
+		
+		DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject().put("http.port", 8080)
+				.put("url", "jdbc:hsqldb:mem:test?shutdown=true").put("driver_class", "org.hsqldb.jdbcDriver"));
 		Vertx vertx = Vertx.vertx();
-		vertx.deployVerticle(MyFirstVerticle.class.getName());
+		vertx.deployVerticle(MyFirstVerticle.class.getName(), options);
 	}
 
 }
